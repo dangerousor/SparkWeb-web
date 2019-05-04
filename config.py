0e8c3792841f549320424a0bb1ae5c343e69065d
dangerousor <1192350292@qq.com>
2019-05-04 16:56:09 +0800

    spark

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:liuxiaofeng@localhost:3306/SparkWebProject?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:liuxiaofeng@localhost:3306/sparkwebproject?charset=utf8'
 # UPLOAD_FOLDER = 'E:\\tmp'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True

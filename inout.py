2727d7dfadab880fb804c864e8dcb7cf2554c385
dangerousor <1192350292@qq.com>
2019-05-05 11:15:34 +0800

    data

--- inout.py
+++ inout.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-in0out1 = ["sql-instream", "model-instream", "data-instream", ]
-in1out0 = ["data-outstream", "sql-outstream", "model-outstream", ]
-in1out1 = ["kmeans", "fpgrowth", "logistic-regression", "sort", "random", "sql-execute", "normalization", "fillna", "drop_duplicate", "dropna", ]
-in1out2 = ["split-col", "split-row", ]
-in2out1 = ["predict", "merge-row", "merge-col", ]
-in2out2 = []
+in0out1 = [
+    # "sql-instream",
+    "model-instream",
+    "data-instream",
+]
+in1out0 = [
+    "data-outstream",
+    # "sql-outstream",
+    "model-outstream",
+]
+in1out1 = [
+    "kmeans",
+    "fpgrowth",
+    "logistic-regression",
+    "sort",
+    "random",
+    "normalization",
+    "split-col",
+]
+in1out2 = [
+]
+in2out1 = [
+    "predict",
+    "merge-row",
+    "merge-col",
+]
+in2out2 = [
+
+]
